4d445f9d438feaf644f7a14131c93d59e17ccb2a
Micaela A. Rossi <micaelarrossi@gmail.com>
2023-05-02 21:01:53 -0300

    BaSP-W07-2023: Reverted the try for create the modals. correction of index.html some links does't redirected correctly

--- Week-07/js/login.js
+++ Week-07/js/login.js
@@ -87,70 +87,20 @@ window.onload = function(){
         }
       });
 
-      var modalTextSuccess = document.getElementsByClassName('modal-text-success');
-      var modalTextError = document.getElementsByClassName('modal-text-error');
-
-      function hideCloseModals(){
-         var hideWhenClosed = document.getElementsByClassName('hide-when-closed');
-        for (var i = 0; i < hideWhenClosed.length; i++) {
-            hideWhenClosed[i].style.display = 'none';
-        }
-      }
-
-      function showAlertSuccess() {
-        var modalSuccess = document.getElementById('modal-container-success');
-        modalSuccess.style.display = 'block';
-        var closeCrossSuccess = document.getElementsByClassName('close-success')[0];
-        closeCrossSuccess.onclick = function () {
-            modalTextSuccess.innerText = ''
-            modalSuccess.style.display = 'none';
-            hideCloseModals();
-        };
-        var closeButtonSuccess = document.getElementsByClassName('close-button-success')[0];
-        closeButtonSuccess.onclick = function () {
-            modalTextSuccess.innerText = ''
-            modalSuccess.style.display = 'none';
-            hideCloseModals();
-        };
-      }
-
-      function showAlertError() {
-        var modalError = document.getElementById('modal-container-error');
-        modalError.style.display = 'block';
-        var closeCrossError = document.getElementsByClassName('close-error')[0];
-        closeCrossError.onclick = function () {
-            modalTextError.innerText = ''
-            modalError.style.display = 'none';
-            hideCloseModals();
-        };
-        var closeButtonError = document.getElementsByClassName('close-button-error')[0];
-        closeButtonError.onclick = function () {
-            modalTextError.innerText = ''
-            modalError.style.display = 'none';
-            hideCloseModals();
-        };
-      }
-
     logInButton.addEventListener('click', function(event){
         event.preventDefault();
         emailCheck();
         passwordCheck();
         if(!passwordInput.classList.contains('valid-field') && !emailInput.classList.contains('valid-field')){
-            modalTextError.innerText = 'Log in error!! \nPlease check the email and password fields.'
-            showAlertError()
-            //alert('Log in error!! \nPlease check the email and password fields.');
+            alert('Log in error!! \nPlease check the email and password fields.');
             return;
         }
         if (!passwordInput.classList.contains('valid-field')) {
-            modalTextError.innerText = 'Please check the password field.'
-            showAlertError()
-            //alert('Please check the password field.');
+            alert('Please check the password field.');
             return;
         }
         if (!emailInput.classList.contains('valid-field')) {
-            modalTextError.innerText = 'Please check the email field.'
-            showAlertError()
-            //alert('Please check the email field.');
+            alert('Please check the email field.');
             return;
         }
         var apiUrl = 'https://api-rest-server.vercel.app/login?email=' + emailInput.value + '&password=' + passwordInput.value;
@@ -163,19 +113,13 @@ window.onload = function(){
             })
             .then(function(data){
                 if (data.success){
-                    modalTextSuccess.innerText = 'Succes: ' + data.msg +'\nWelcome ' + emailInput.value
-                    showAlertSuccess()
-                    //alert('Succes: ' + data.msg +'\nWelcome ' + emailInput.value)
+                    alert('Succes: ' + data.msg +'\nWelcome ' + emailInput.value)
                 }else {
-                    modalTextError.innerText = 'Error: ' + data
-                    showAlertError()
-                    //alert('Error: ' + data)
+                    alert('Error: ' + data)
                 }
             })
             .catch(function(error){
-                modalTextError.innerText = 'Something goes wrong'
-                showAlertError()
-                //alert('Something goes wrong');
+                alert('Something goes wrong');
             });
     });
 }
